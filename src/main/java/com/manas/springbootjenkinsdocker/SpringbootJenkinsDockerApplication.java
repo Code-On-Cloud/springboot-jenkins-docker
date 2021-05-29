a0a7b3463b1f8fc254d8edbfcf41f132c03d0a79
Githubmax21 <manashait87@gmail.com>
2021-05-29 14:28:11 +0530

    first commit

---
+++ src/main/java/com/manas/springbootjenkinsdocker/SpringbootJenkinsDockerApplication.java
@@ -0,0 +1,25 @@
+package com.manas.springbootjenkinsdocker;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDockerApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringbootJenkinsDockerApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("My application started  !!!!");
+	}
+	
+	public static void main(String[] args) {
+		logger.info("My application executed  !!!!");
+		SpringApplication.run(SpringbootJenkinsDockerApplication.class, args);
+	}
+
+}
