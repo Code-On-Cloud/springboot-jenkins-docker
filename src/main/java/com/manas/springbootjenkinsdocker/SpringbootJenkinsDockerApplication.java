e3903ce3eab26def6f69eb65181c3b35a7086fb3
Githubmax21 <manashait87@gmail.com>
2021-05-31 08:47:40 +0530

    adding controller

--- src/main/java/com/manas/springbootjenkinsdocker/SpringbootJenkinsDockerApplication.java
+++ src/main/java/com/manas/springbootjenkinsdocker/SpringbootJenkinsDockerApplication.java
@@ -6,11 +6,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootJenkinsDockerApplication {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringbootJenkinsDockerApplication.class);
+	
+	@GetMapping
+	public String getMessage() {
+		return "Hi... This is a SpringBoot project";
+	} 
 	
 	@PostConstruct
 	public void init() {
